7c08b55fe9c534461e8e63004487779630c088f7
Jahnavi Dasari <109128859+jahnavidasari10@users.noreply.github.com>
2024-02-13 21:10:31 +0530

    Add files via upload

---
+++ leetcode-13-2-24.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for(auto s:words){
+         string r(rbegin(s),rend(s));
+         if(r==s)
+             return s;
+        }
+        return "";
+    }
+};
