9337077eef86f31da160c9e52a5600a4beb030bf
Jahnavi Dasari <109128859+jahnavidasari10@users.noreply.github.com>
2024-03-24 22:20:46 +0530

    Add files via upload

---
+++ leetcode-24-3-24.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        for(auto i:mp){
+            if(i.second>1){
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
