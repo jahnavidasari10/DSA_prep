aee0fc25979943f2fb6869dc473169caf331b31c
Jahnavi Dasari <109128859+jahnavidasari10@users.noreply.github.com>
2024-04-18 23:05:23 +0530

    Add files via upload

---
+++ gfg-18-4-24.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    //Function to find two repeated elements.
+    vector<int> twoRepeated (int a[], int n) {
+        vector<int>v(n+2),ans;
+        for(int i=0;i<n+2;i++){
+           if(v[a[i]])ans.push_back(a[i]);
+           v[a[i]]=1;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    
+    while(t--)
+    {
+        cin>>n;
+        
+        int a[n+2];
+        
+        for(int i=0;i<n+2;i++)
+            cin>>a[i];
+        
+        Solution obj;
+        vector<int> res;
+        res = obj.twoRepeated(a, n);
+        cout<<res[0]<<" "<<res[1]<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
