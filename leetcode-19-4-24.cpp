d19a1b1254b9496fec1affae9780ba16450f128a
Jahnavi Dasari <109128859+jahnavidasari10@users.noreply.github.com>
2024-04-19 22:12:36 +0530

    Add files via upload

---
+++ leetcode-19-4-24.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&grid){
+        vis[row][col]=1;
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,-1,0,1};
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && 
+                grid[nrow][ncol]=='1'){
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'&& !vis[i][j]){
+                    ans++;
+                    bfs(i,j,vis,grid);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
