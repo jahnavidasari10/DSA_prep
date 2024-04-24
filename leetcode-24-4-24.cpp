46a1f963831268f75e3e56ab63815a0c9a128d99
Jahnavi Dasari <109128859+jahnavidasari10@users.noreply.github.com>
2024-04-24 23:42:39 +0530

    Add files via upload

---
+++ leetcode-24-4-24.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    int solveTab(int n){
+        vector<int>dp(n+1,0);
+        dp[0]=0;
+        dp[1]=dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
+    }
+    int tribonacci(int n) {
+       if(n<3){
+           return n>0? 1:0;
+       }
+       return solveTab(n);
+    }
+};
